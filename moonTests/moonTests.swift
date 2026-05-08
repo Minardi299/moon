7bed8cdc7a05473332ff3b20a88030bbf798b681
Minardi299 <phu.ngo@outlook.com>
2026-05-07 21:55:28 -0400

    Initial Commit

---
+++ moonTests/moonTests.swift
@@ -0,0 +1,16 @@
+//
+//  moonTests.swift
+//  moonTests
+//
+//  Created by Minh on 2026-05-07.
+//
+
+import Testing
+
+struct moonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
